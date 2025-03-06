bb16acef91d3aa71d4870ba38bd62b9b03e02733
drb1010 <deepak.bhattarai@omegabpo.com>
2025-03-06 18:39:57 +0545

    changed the phone number field code

--- lib/screens/signUp/presenttaion/signupPage.dart
+++ lib/screens/signUp/presenttaion/signupPage.dart
@@ -14,7 +14,6 @@ import 'package:image_picker/image_picker.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
 import 'package:rooster_empployee/utils/tostMessage.dart';
 
-
 class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
 
@@ -34,8 +33,8 @@ class _SignupPageState extends State<SignupPage> {
   final phoneNumberkey = GlobalKey<FormFieldState>();
   final emailKey = GlobalKey<FormFieldState>();
   final locationKey = GlobalKey<FormFieldState>();
-  final passwordKey= GlobalKey<FormFieldState>();
-  final confirmPasswordKey= GlobalKey<FormFieldState>();
+  final passwordKey = GlobalKey<FormFieldState>();
+  final confirmPasswordKey = GlobalKey<FormFieldState>();
 
   File? _profileImage;
   String _countryCode = '+1';
@@ -69,13 +68,21 @@ class _SignupPageState extends State<SignupPage> {
             print('the user data is ${state.userData}');
             onSuccesfullSignup(state.userData);
             // Navigator.pushReplacementNamed(context, Routes.);
-            Navigator.of(context).push(MaterialPageRoute(builder: (context) => OtpVerificationPage(phoneNumber: '${_countryCode}${_phone.text.trim()}',)));
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => OtpVerificationPage(
+                      phoneNumber: '${_countryCode}${_phone.text.trim()}',
+                    )));
           }
         },
         builder: (context, state) {
           return Scaffold(
             backgroundColor: AppColors.primary,
-            appBar: AppBar(title:Text('Sign Up',) ,centerTitle: true, ),
+            appBar: AppBar(
+              title: Text(
+                'Sign Up',
+              ),
+              centerTitle: true,
+            ),
             body: PopScope(
               child: Stack(
                 children: [
@@ -114,15 +121,15 @@ class _SignupPageState extends State<SignupPage> {
             backgroundImage:
                 _profileImage != null ? FileImage(_profileImage!) : null,
             child: _profileImage == null
-                ? Icon(Icons.camera_alt, size: 40.r, color: AppColors.textPrimary)
+                ? Icon(Icons.camera_alt,
+                    size: 40.r, color: AppColors.textPrimary)
                 : null,
           ),
         ),
         SizedBox(height: 10.h),
         Text(
           'Add Profile Photo',
-          style: AppTextStyles.bodySmall
-              .copyWith(color: AppColors.surface),
+          style: AppTextStyles.bodySmall.copyWith(color: AppColors.surface),
         ),
       ],
     );
@@ -137,9 +144,10 @@ class _SignupPageState extends State<SignupPage> {
       ),
       child: Column(
         children: [
-          Text('Create Account', style: AppTextStyles.headline2.copyWith(color: Colors.white)),
+          Text('Create Account',
+              style: AppTextStyles.headline2.copyWith(color: Colors.white)),
           SizedBox(height: 20.h),
-          _buildNameFields(firstNamekey,lastNamekey),
+          _buildNameFields(firstNamekey, lastNamekey),
           SizedBox(height: 15.h),
           _buildEmailField(emailKey),
           SizedBox(height: 15.h),
@@ -150,7 +158,6 @@ class _SignupPageState extends State<SignupPage> {
           _buildPhoneField(),
           SizedBox(height: 15.h),
           _buildLocationField(locationKey),
-        
           SizedBox(height: 20.h),
           _buildSubmitButton(),
         ],
@@ -160,10 +167,10 @@ class _SignupPageState extends State<SignupPage> {
 
   Widget _buildPasswordField(GlobalKey<FormFieldState> passwordkey) {
     return TextFormField(
-       key: passwordkey,
-            onChanged: ((value){
-              passwordkey.currentState!.validate();
-            }),
+      key: passwordkey,
+      onChanged: ((value) {
+        passwordkey.currentState!.validate();
+      }),
       controller: _password,
       obscureText: !_passwordVisible,
       decoration: InputDecoration(
@@ -187,11 +194,10 @@ class _SignupPageState extends State<SignupPage> {
 
   Widget _buildConfirmPasswordField(GlobalKey<FormFieldState> conpasswordkey) {
     return TextFormField(
-     
-       key: conpasswordkey,
-            onChanged: ((value){
-              conpasswordkey.currentState!.validate();
-            }),
+      key: conpasswordkey,
+      onChanged: ((value) {
+        conpasswordkey.currentState!.validate();
+      }),
       controller: _confirmPassword,
       obscureText: !_confirmPasswordVisible,
       decoration: InputDecoration(
@@ -213,13 +219,14 @@ class _SignupPageState extends State<SignupPage> {
     );
   }
 
-  Widget _buildNameFields(GlobalKey<FormFieldState> firstnamekey, GlobalKey<FormFieldState> lastNamekey) {
+  Widget _buildNameFields(GlobalKey<FormFieldState> firstnamekey,
+      GlobalKey<FormFieldState> lastNamekey) {
     return Row(
       children: [
         Expanded(
           child: TextFormField(
             key: firstnamekey,
-            onChanged: ((value){
+            onChanged: ((value) {
               firstnamekey.currentState!.validate();
             }),
             controller: _firstName,
@@ -230,8 +237,8 @@ class _SignupPageState extends State<SignupPage> {
         SizedBox(width: 10.w),
         Expanded(
           child: TextFormField(
-             key: lastNamekey,
-            onChanged: ((value){
+            key: lastNamekey,
+            onChanged: ((value) {
               lastNamekey.currentState!.validate();
             }),
             controller: _lastName,
@@ -245,12 +252,11 @@ class _SignupPageState extends State<SignupPage> {
 
   Widget _buildEmailField(GlobalKey<FormFieldState> itemkey) {
     return TextFormField(
-      
       controller: _email,
-       key: itemkey,
-            onChanged: ((value){
-              itemkey.currentState!.validate();
-            }),
+      key: itemkey,
+      onChanged: ((value) {
+        itemkey.currentState!.validate();
+      }),
       decoration: const InputDecoration(labelText: 'Email'),
       validator: (value) =>
           !RegExp(r"^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
@@ -263,24 +269,31 @@ class _SignupPageState extends State<SignupPage> {
   Widget _buildPhoneField() {
     return IntlPhoneField(
       controller: _phone,
-      initialCountryCode: 'US',
+      initialCountryCode: _countryCode,
       onChanged: (phone) {
-        _countryCode = phone.countryCode;
-        _phoneNumber = phone.number;
+        setState(() {
+          _countryCode = phone.countryISOCode;
+          _phoneNumber = phone.number;
+        });
+        print('countru code : ${phone.countryCode}');
       },
-      decoration: InputDecoration(
+      onCountryChanged: (value) {
+        setState(() {
+          _countryCode = value.code;
+        });
+      },
+      decoration: const InputDecoration(
         labelText: 'Phone Number',
-        
       ),
     );
   }
 
   Widget _buildLocationField(GlobalKey<FormFieldState> itemkey) {
     return TextFormField(
-       key: itemkey,
-            onChanged: ((value){
-              itemkey.currentState!.validate();
-            }),
+      key: itemkey,
+      onChanged: ((value) {
+        itemkey.currentState!.validate();
+      }),
       controller: _location,
       decoration: InputDecoration(
         labelText: 'Location',
@@ -295,7 +308,6 @@ class _SignupPageState extends State<SignupPage> {
     );
   }
 
- 
   Widget _buildSubmitButton() {
     return ElevatedButton(
       onPressed: _submitForm,
@@ -317,102 +329,113 @@ class _SignupPageState extends State<SignupPage> {
         'profileImage': _profileImage?.path,
       };
 
-      context.read<SignupBloc>().add(SignupUser(
-          firstName: _firstName.text.trim(),
-          countyCode: _countryCode.trim(),
-          lastName: _lastName.text.trim(),
-          email: _email.text.trim(),
-          phone: '$_countryCode ${_phone.text.trim()}',
-          location: _location.text.trim(),
-          photo: _profileImage?.path,
-          password: _password.text.trim()),
+      context.read<SignupBloc>().add(
+            SignupUser(
+                firstName: _firstName.text.trim(),
+                countyCode: _countryCode.trim(),
+                lastName: _lastName.text.trim(),
+                email: _email.text.trim(),
+                phone: '$_countryCode ${_phone.text.trim()}',
+                location: _location.text.trim(),
+                photo: _profileImage?.path,
+                password: _password.text.trim()),
           );
     }
   }
 }
 
-void onSuccesfullSignup(Map<String, String?>  userData     ){
+void onSuccesfullSignup(Map<String, String?> userData) {
   print('the user data in function is $userData');
-FlutterSecureData.deleteWholeSecureData().then((value){
-  FlutterSecureData.setIsLoggedIn('true');
-  FlutterSecureData.setPassword(userData['password']??'');
-  FlutterSecureData.setUserName(userData['firstName']??'');
-  FlutterSecureData.setUserData(jsonEncode(userData));
-
+  FlutterSecureData.deleteWholeSecureData().then((value) {
+    FlutterSecureData.setIsLoggedIn('true');
+    FlutterSecureData.setPassword(userData['password'] ?? '');
+    FlutterSecureData.setUserName(userData['firstName'] ?? '');
+    FlutterSecureData.setUserData(jsonEncode(userData));
 
     final doneTasks = [
-  {
-    'employeeName': '${userData['firstName']??'name'} ${userData['lastName']??''}',
-    'employeeEmail': userData['email']??'email',
-    'employeeId': 'ROO-2345',
-    'taskTitle': 'Client Meeting Preparation',
-    'taskDescription': 'Prepare presentation and documents for client meeting',
-    'location': 'Office Building A, Floor 12',
-    'startTime': DateTime(2025, 2, 3, 20, 10).toString(),
-    'endTime': DateTime(2025, 2, 3, 20, 15).toString(),
-    'isLate':'false'
-  },
-  {
-      'employeeName': '${userData['firstName']??'name'} ${userData['lastName']??''}',
-    'employeeEmail': userData['email']??'email',
-    'employeeId': 'ROO-2345',
-    'taskTitle': 'Team Strategy Discussion',
-    'taskDescription': 'Discuss strategy for upcoming project and milestones',
-    'location': 'Office Building B, Conference Room 3',
-    'startTime': DateTime(2025, 2, 4, 9, 30).toString(),
-    'endTime': DateTime(2025, 2, 4, 10, 30).toString(),
-    'isLate':'false'
-  },
-  {
-    'employeeName': '${userData['firstName']??'name'} ${userData['lastName']??''}',
-    'employeeEmail': userData['email']??'email',
-    'employeeId': 'ROO-2345',
-    'taskTitle': 'Client Call',
-    'taskDescription': 'Call client to discuss project updates and deadlines',
-    'location': 'Remote',
-    'startTime': DateTime(2025, 2, 5, 15, 00).toString(),
-    'endTime': DateTime(2025, 2, 5, 15, 30).toString(),
-    'isLate':'false'
-  },
-  {
-     'employeeName': '${userData['firstName']??'name'} ${userData['lastName']??''}',
-    'employeeEmail': userData['email']??'email',
-    'employeeId': 'ROO-2345',
-    'taskTitle': 'Internal Meeting',
-    'taskDescription': 'Discuss progress on internal project and team performance',
-    'location': 'Office Building A, Floor 10',
-    'startTime': DateTime(2025, 2, 6, 11, 00).toString(),
-    'endTime': DateTime(2025, 2, 6, 12, 00).toString(),
-    'isLate':'false'
-  },
-  {
-      'employeeName': '${userData['firstName']??'name'} ${userData['lastName']??''}',
-    'employeeEmail': userData['email']??'email',
-    'employeeId': 'ROO-2345',
-    'taskTitle': 'Workshop Preparation',
-    'taskDescription': 'Prepare slides and materials for the upcoming workshop',
-    'location': 'Office Building C, Training Room 5',
-    'startTime': DateTime(2025, 2, 7, 8, 45).toString(),
-    'endTime': DateTime(2025, 2, 7, 9, 30).toString(),
-    'isLate':'false'
-  },
-  {
-     'employeeName': '${userData['firstName']??'name'} ${userData['lastName']??''}',
-    'employeeEmail': userData['email']??'email',
-    'employeeId': 'ROO-2345',
-    'taskTitle': 'Project Review Meeting',
-    'taskDescription': 'Review project progress and set next steps with team',
-    'location': 'Office Building B, Conference Room 1',
-    'startTime': DateTime(2025, 2, 8, 14, 00).toString(),
-    'endTime': DateTime(2025, 2, 8, 14, 45).toString(),
-    'isLate':'false'
-  },
-];
+      {
+        'employeeName':
+            '${userData['firstName'] ?? 'name'} ${userData['lastName'] ?? ''}',
+        'employeeEmail': userData['email'] ?? 'email',
+        'employeeId': 'ROO-2345',
+        'taskTitle': 'Client Meeting Preparation',
+        'taskDescription':
+            'Prepare presentation and documents for client meeting',
+        'location': 'Office Building A, Floor 12',
+        'startTime': DateTime(2025, 2, 3, 20, 10).toString(),
+        'endTime': DateTime(2025, 2, 3, 20, 15).toString(),
+        'isLate': 'false'
+      },
+      {
+        'employeeName':
+            '${userData['firstName'] ?? 'name'} ${userData['lastName'] ?? ''}',
+        'employeeEmail': userData['email'] ?? 'email',
+        'employeeId': 'ROO-2345',
+        'taskTitle': 'Team Strategy Discussion',
+        'taskDescription':
+            'Discuss strategy for upcoming project and milestones',
+        'location': 'Office Building B, Conference Room 3',
+        'startTime': DateTime(2025, 2, 4, 9, 30).toString(),
+        'endTime': DateTime(2025, 2, 4, 10, 30).toString(),
+        'isLate': 'false'
+      },
+      {
+        'employeeName':
+            '${userData['firstName'] ?? 'name'} ${userData['lastName'] ?? ''}',
+        'employeeEmail': userData['email'] ?? 'email',
+        'employeeId': 'ROO-2345',
+        'taskTitle': 'Client Call',
+        'taskDescription':
+            'Call client to discuss project updates and deadlines',
+        'location': 'Remote',
+        'startTime': DateTime(2025, 2, 5, 15, 00).toString(),
+        'endTime': DateTime(2025, 2, 5, 15, 30).toString(),
+        'isLate': 'false'
+      },
+      {
+        'employeeName':
+            '${userData['firstName'] ?? 'name'} ${userData['lastName'] ?? ''}',
+        'employeeEmail': userData['email'] ?? 'email',
+        'employeeId': 'ROO-2345',
+        'taskTitle': 'Internal Meeting',
+        'taskDescription':
+            'Discuss progress on internal project and team performance',
+        'location': 'Office Building A, Floor 10',
+        'startTime': DateTime(2025, 2, 6, 11, 00).toString(),
+        'endTime': DateTime(2025, 2, 6, 12, 00).toString(),
+        'isLate': 'false'
+      },
+      {
+        'employeeName':
+            '${userData['firstName'] ?? 'name'} ${userData['lastName'] ?? ''}',
+        'employeeEmail': userData['email'] ?? 'email',
+        'employeeId': 'ROO-2345',
+        'taskTitle': 'Workshop Preparation',
+        'taskDescription':
+            'Prepare slides and materials for the upcoming workshop',
+        'location': 'Office Building C, Training Room 5',
+        'startTime': DateTime(2025, 2, 7, 8, 45).toString(),
+        'endTime': DateTime(2025, 2, 7, 9, 30).toString(),
+        'isLate': 'false'
+      },
+      {
+        'employeeName':
+            '${userData['firstName'] ?? 'name'} ${userData['lastName'] ?? ''}',
+        'employeeEmail': userData['email'] ?? 'email',
+        'employeeId': 'ROO-2345',
+        'taskTitle': 'Project Review Meeting',
+        'taskDescription':
+            'Review project progress and set next steps with team',
+        'location': 'Office Building B, Conference Room 1',
+        'startTime': DateTime(2025, 2, 8, 14, 00).toString(),
+        'endTime': DateTime(2025, 2, 8, 14, 45).toString(),
+        'isLate': 'false'
+      },
+    ];
 
+    FlutterSecureData.setDoneTask(jsonEncode(doneTasks));
+    FlutterSecureData.setTodo('true');
 
-FlutterSecureData.setDoneTask(jsonEncode(doneTasks));
-FlutterSecureData.setTodo('true');
-
-  // FlutterSecureData.setProfileImage(image);
-});
+    // FlutterSecureData.setProfileImage(image);
+  });
 }
